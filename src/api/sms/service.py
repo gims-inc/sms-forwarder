43a5dc2f93aafe066169a2e5660dc65f4aa57634
dev0 <gabrielmusanga@gmail.com>
2026-08-24 23:44:55 +0300

    gitignore

--- src/api/sms/service.py
+++ src/api/sms/service.py
@@ -1,39 +1,10 @@
 import os
-from datetime import datetime
-
 import aiosqlite
+from utils.utils import parse_time
 
 DB_PATH = os.getenv("SMS_DB", "sms.db")
 
 
-class utils:
-    @staticmethod
-    def parse_time(raw: str) -> str:
-        raw = raw.strip()
-        formats = [
-            "%Y-%m-%d %H:%M:%S",
-            "%m/%d/%y, %I:%M %p",
-            "%m/%d/%y %I:%M %p",
-            "%m/%d/%y, %H:%M",
-            "%m/%d/%y %H:%M",
-            "%Y-%m-%dT%H:%M:%S",
-        ]
-
-        for fmt in formats:
-            try:
-                return datetime.strptime(raw, fmt).strftime("%Y-%m-%d %H:%M:%S")
-            except ValueError:
-                continue
-
-        try:
-            year = datetime.now().year
-            return datetime.strptime(f"{year}/{raw}", "%Y/%m/%d, %I:%M %p").strftime(
-                "%Y-%m-%d %H:%M:%S"
-            )
-        except ValueError:
-            pass
-
-        return raw
 
 
 class SmsMessageService:
